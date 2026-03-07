e17401806afd9d03942b94944032ab783bc716ab
Matt Howarth <msh32@njit.edu>
2026-03-07 00:01:47 -0500

    Update app/calculation.py and test_calculation.py with all calculations. Remove REPL to pass pytest

--- app/calculation.py
+++ app/calculation.py
@@ -1,4 +1,5 @@
 """Calculations"""
+import math
 
 def add(a: float, b: float) -> float:
     return a + b 
@@ -13,3 +14,21 @@ def divide(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+def power(a: float, b: float) -> float:
+    return a ** b
+
+def root(a: float, b: float) -> float:
+    return a ** (1/b)
+
+def modulus(a: float, b: float) -> float:
+    return a % b
+
+def int_divide(a: float, b: float) -> float:
+    return a // b
+
+def percent(a: float, b: float) -> float:
+    return (a / b)*100 
+
+def abs_diff(a: float, b: float) -> float:
+    return abs(a - b)
--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculation import add, subtract, multiply, divide
+from app.calculation import add, subtract, multiply, divide, power, root, modulus, int_divide, percent, abs_diff
 
 def test_add():
     assert add(1,1) == 2
@@ -16,3 +16,21 @@ def test_division():
 def test_divide_with_zero():
     with pytest.raises(ValueError, match="Cannot divide by zero"):
         divide(1, 0)
+
+def test_power():
+    assert power(2,2) == 4
+
+def test_root():
+    assert root(36, 2) == 6 
+
+def test_modulus():
+    assert modulus(13, 3) == 1
+
+def test_int_divide():
+    assert int_divide(10, 3) == 3
+
+def test_percent():
+    assert percent(10, 100) == 10 
+
+def test_abs_diff():
+    assert abs_diff(4, 5) == 1
