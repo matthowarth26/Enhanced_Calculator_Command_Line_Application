ff1aee64e5c9d8b571b3b92713ed3a05b6b938b0
Matt Howarth <msh32@njit.edu>
2026-03-07 18:51:11 -0500

    Added tests for max input validation for absolute values below and above threshold

--- tests/test_input_validators.py
+++ tests/test_input_validators.py
@@ -1,6 +1,6 @@
 import pytest
 from app.exceptions import ValidationError
-from app.input_validators import validate_input_is_number
+from app.input_validators import validate_input_is_number, validate_max_input_value
 
 def test_validate_input_is_number_with_int_string():
     assert validate_input_is_number("5") == 5.0
@@ -14,3 +14,17 @@ def test_validate_input_is_number_with_int():
 def test_validate_input_is_number_with_invalid_string():
     with pytest.raises(ValidationError, match="Invalid number: five"):
         validate_input_is_number("five")
+
+def test_validate_max_input_value_valid():
+    assert validate_max_input_value(100) == 100
+
+def test_validate_max_input_value_negative_valid():
+    assert validate_max_input_value(-100) == -100
+
+def test_validate_max_input_value_invalid():
+    with pytest.raises(ValidationError):
+        validate_max_input_value(2_000_000_000)
+
+def test_validate_max_input_value_negative_invalid():
+    with pytest.raises(ValidationError):
+        validate_max_input_value(2_000_000_000)
