bce80f1a847a41a0512aba5d8e6f52e2e3c8ce9a
Matt Howarth <msh32@njit.edu>
2026-03-08 13:38:27 -0400

    Clean up REPL with Calculatory Factory method, call class list instead of writing them out, and update order of operations

--- app/calculator.py
+++ app/calculator.py
@@ -10,18 +10,7 @@ def Calculator():
     print("Welcome to the calculator app!")
 
     # List of possible operations 
-    operation_list = [
-        "add",
-        "subtract",
-        "multiply",
-        "divide",
-        "power",
-        "root",
-        "modulus",
-        "integer divide",
-        "percent",
-        "absolute difference",
-    ]
+    operation_list = CalculationFactory.get_supported_operations()
 
     # Start Loop 
     while True: 
@@ -33,37 +22,27 @@ def Calculator():
             break 
         
         # Check if user inputs one of the possible operations 
-        if user_input in operation_list:
-            print("Calculator takes two numbers:")
-            
-            # Prompt user to input both numbers 
-            a = input("First number: ")
-            b = input("Second number: ")
-
-            # Check if user tries to exit 
-            if a.lower() == "exit" or b.lower() == "exit":
-                print("Exiting REPL")
-                break 
-            
-            # Validate that both inputs are valid numbers and do not exceed max limit 
-            try: 
-                a, b = validate_two_valid_inputs(a, b)
-
-            except ValidationError as e:
-                print(f"Error: {e}")
-                continue 
-            
-            # Use CalculationFactory method for inputted operation  
-            try:
-                calculation = CalculationFactory.create_calculation(user_input, a, b)
-                result = calculation.compute()
-
-                print(f"Result: {result}")
-            
-            except OperationError as e:
-                print(f"Error: {e}")
+        if user_input not in operation_list:
+            print("Please choose from the list of available commands: " + ", ".join(operation_list)+".")
+            continue 
         
-        else:   
-            print("Please choose from the list of available commands: " \
-            "add, subtract, multiply, divide, power, root, modulus, integer divide, percent, absolute difference.")
+        print("Calculator takes two numbers:")
+            
+        # Prompt user to input both numbers 
+        a = input("First number: ")
+        b = input("Second number: ")
 
+        # Check if user tries to exit 
+        if a.lower() == "exit" or b.lower() == "exit":
+            print("Exiting REPL")
+            break 
+            
+        # Validate that both inputs are valid numbers and do not exceed max limit 
+        try: 
+            a, b = validate_two_valid_inputs(a, b)
+            calculation = CalculationFactory.create_calculation(user_input, a, b) # Use CalculationFactory method for inputted operation  
+            result = calculation.compute()
+            print(f"Result: {result}")
+            
+        except (ValidationError, OperationError) as e:
+            print(f"Error: {e}")
