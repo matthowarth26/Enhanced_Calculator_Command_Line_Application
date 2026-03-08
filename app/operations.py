06cb4251b5358d85c463a165f62c1021bd12e43a
Matt Howarth <msh32@njit.edu>
2026-03-07 21:29:26 -0500

    Update to add Operation class and static methods for calculations

--- app/operations.py
+++ app/operations.py
@@ -1,42 +1,56 @@
-"""Calculations"""
+"""Operations"""
 from app.exceptions import OperationError
 
-def add(a: float, b: float) -> float:
-    return a + b 
+class Operation:
 
-def subtract(a: float, b: float) -> float:
-    return a - b 
+    @staticmethod
+    def add(a: float, b: float) -> float:
+        return a + b 
 
-def multiply(a: float, b: float) -> float:
-    return a * b 
+    @staticmethod
+    def subtract(a: float, b: float) -> float:
+        return a - b 
 
-def divide(a: float, b: float) -> float:
-    if b == 0:
-        raise OperationError("Cannot perform division by zero")
-    return a / b
+    @staticmethod
+    def multiply(a: float, b: float) -> float:
+        return a * b 
 
-def power(a: float, b: float) -> float:
-    return a ** b
+    @staticmethod
+    def divide(a: float, b: float) -> float:
+        if b == 0:
+            raise OperationError("Cannot perform division by zero")
+        return a / b
 
-def root(a: float, b: float) -> float:
-    if b == 0:
-        raise OperationError("Cannot perform root with degree zero")
-    return a ** (1/b)
+    @staticmethod
+    def power(a: float, b: float) -> float:
+        return a ** b
 
-def modulus(a: float, b: float) -> float:
-    if b == 0:
-        raise OperationError("Cannot perform modulus by zero")
-    return a % b
+    @staticmethod
+    def root(a: float, b: float) -> float:
+        if b == 0:
+            raise OperationError("Cannot perform root with degree zero")
+        if a < 0:
+            raise OperationError("Cannot perform root with negative number")
+        return a ** (1/b)
 
-def int_divide(a: float, b: float) -> float:
-    if b == 0:
-        raise OperationError("Cannot perform integer division by zero")
-    return a // b
+    @staticmethod
+    def modulus(a: float, b: float) -> float:
+        if b == 0:
+            raise OperationError("Cannot perform modulus by zero")
+        return a % b
 
-def percent(a: float, b: float) -> float:
-    if b == 0:
-        raise OperationError("Cannot perform division by zero")    
-    return (a / b)*100 
+    @staticmethod
+    def int_divide(a: float, b: float) -> float:
+        if b == 0:
+            raise OperationError("Cannot perform integer division by zero")
+        return a // b
 
-def abs_diff(a: float, b: float) -> float:
-    return abs(a - b)
+    @staticmethod
+    def percent(a: float, b: float) -> float:
+        if b == 0:
+            raise OperationError("Cannot perform division by zero")    
+        return (a / b)*100 
+
+    @staticmethod
+    def abs_diff(a: float, b: float) -> float:
+        return abs(a - b)
