c898b0006ac0c8a6e4484da9958888abd716d25d
Matt Howarth <msh32@njit.edu>
2026-03-07 21:59:10 -0500

    Add abstract calculation base class and operation subclasses

---
+++ app/calculation.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+from app.operations import Operation
+
+# Abstract Class Method for calculations 
+class Calculation(ABC):
+    
+    def __init__(self, a: float, b: float) -> None:
+        self.a = a 
+        self.b = b
+
+    @abstractmethod
+    def compute(self) -> float:
+        pass 
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.a}, {self.b})"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(a={self.a}, b={self.b})"
+
+# Calculation Factory 
+class Addition(Calculation):
+    def compute(self):
+        return Operation.add(self.a, self.b)
+
+class Subtraction(Calculation):
+    def compute(self):
+        return Operation.subtract(self.a, self.b)
+
+class Multiplication(Calculation):
+    def compute(self):
+        return Operation.multiply(self.a, self.b)
+    
+class Division(Calculation):
+    def compute(self):
+        return Operation.divide(self.a, self.b)
+
+class Power(Calculation):
+    def compute(self):
+        return Operation.power(self.a, self.b)
+    
+class Root(Calculation):
+    def compute(self):
+        return Operation.root(self.a, self.b)
+    
+class Modulus(Calculation):
+    def compute(self):
+        return Operation.modulus(self.a, self.b)
+        
+class IntegerDivision(Calculation):
+    def compute(self):
+        return Operation.int_divide(self.a, self.b)
+    
+class Percentage(Calculation):
+    def compute(self):
+        return Operation.percent(self.a, self.b)
+    
+class AbsoluteDifference(Calculation):
+    def compute(self):
+        return Operation.abs_diff(self.a, self.b)
