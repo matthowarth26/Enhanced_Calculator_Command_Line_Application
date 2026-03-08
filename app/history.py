c6c6c8a82472d2e352e0d77171c4caa4554f2954
Matt Howarth <msh32@njit.edu>
2026-03-08 19:01:01 -0400

    Add undo and redo functionality to the history class

--- app/history.py
+++ app/history.py
@@ -11,12 +11,14 @@ class History:
         Initialize a list to store history of operations
         """
         self._history: list[Calculation] = []
+        self._redo_stack: list[Calculation] = []
 
     def add_calculation(self, calculation: Calculation) -> None:
         """
         Add an operation object to history list
         """
         self._history.append(calculation)
+        self._redo_stack.clear()
     
     def get_history(self) -> list[Calculation]:
         """
@@ -29,6 +31,7 @@ class History:
         Clear all operations from history list
         """
         self._history.clear()
+        self._redo_stack.clear()
     
     def is_empty(self) -> bool:
         """
@@ -45,6 +48,37 @@ class History:
         return self._history[-1]
 
     def drop_last_calculation(self) -> Calculation:
+        """
+        Remove most recent operation 
+        """
         if self.is_empty():
             raise HistoryError("Calculator history is empty")
         return self._history.pop()
+    
+    def undo(self) -> Calculation:
+        """"
+        Undo most recent operation by moving it to the redo stack 
+        """
+        if self.is_empty():
+            raise HistoryError("Calculator history is empty - no calculation to undo")
+        
+        calculation = self._history.pop()
+        self._redo_stack.append(calculation)
+        return calculation
+    
+    def redo(self) -> Calculation:
+        """"
+        Redo the most recently undone operation by moving it back to the history list
+        """
+        if not self._redo_stack:
+            raise HistoryError("No calculations to redo")
+        
+        calculation = self._redo_stack.pop()
+        self._history.append(calculation)
+        return calculation
+    
+    def redo_is_empty(self) -> bool: 
+        """
+        Check if redo stack is empty
+        """
+        return len(self._redo_stack) == 0
