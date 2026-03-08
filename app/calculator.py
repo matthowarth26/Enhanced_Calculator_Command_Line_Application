c39d0ff79c0550241661d56df237e4230cb7ba5f
Matt Howarth <msh32@njit.edu>
2026-03-08 11:32:51 -0400

    Replace if elif operation statements with CalculationFactory in REPL

--- app/calculator.py
+++ app/calculator.py
@@ -1,71 +1,69 @@
 """Calulator REPL"""
-from app.operations import Operation
+from app.calculation import CalculationFactory
 from app.exceptions import OperationError, ValidationError
-from app.input_validators import validate_input_is_number, validate_max_input_value, validate_two_valid_inputs
+from app.input_validators import validate_two_valid_inputs
 
 def Calculator():
+    """
+    Calculator REPL for user to input operation and 2 numbers
+    """
     print("Welcome to the calculator app!")
 
-    while True: 
-        user_input = input("Input the command you would like to perform, or type 'exit' to quit: ").lower().strip()
+    # List of possible operations 
+    operation_list = [
+        "add",
+        "subtract",
+        "multiply",
+        "divide",
+        "power",
+        "root",
+        "modulus",
+        "integer divide",
+        "percent",
+        "absolute difference",
+    ]
 
+    # Start Loop 
+    while True: 
+        user_input = input("Input the operation you would like to perform, or type 'exit' to quit: ").lower().strip()
+
+        # Check if user inputs exit 
         if user_input== "exit":
             print("Exiting REPL")
             break 
         
-        if user_input in ['add', 'subtract', 'multiply', 'divide', 'power', 'root', 'modulus', 'integer divide', 'percent', 'absolute difference']:
+        # Check if user inputs one of the possible operations 
+        if user_input in operation_list:
             print("Calculator takes two numbers:")
+            
+            # Prompt user to input both numbers 
             a = input("First number: ")
             b = input("Second number: ")
 
+            # Check if user tries to exit 
             if a.lower() == "exit" or b.lower() == "exit":
                 print("Exiting REPL")
                 break 
             
+            # Validate that both inputs are valid numbers and do not exceed max limit 
             try: 
                 a, b = validate_two_valid_inputs(a, b)
-                b = validate_input_is_number(b)  
 
             except ValidationError as e:
                 print(f"Error: {e}")
                 continue 
             
+            # Use CalculationFactory method for inputted operation  
             try:
-                if user_input == 'add':
-                    result = Operation.add(a, b)
-                
-                elif user_input == 'subtract':
-                    result = Operation.subtract(a, b)
-                
-                elif user_input == 'multiply':
-                    result = Operation.multiply(a, b)
-                
-                elif user_input == 'divide':
-                    result = Operation.divide(a, b)
-                
-                elif user_input == 'power':
-                    result = Operation.power(a, b)      
-                
-                elif user_input == 'root':
-                    result = Operation.root(a, b) 
-                
-                elif user_input == 'modulus':
-                    result = Operation.modulus(a, b)     
-                
-                elif user_input == 'integer divide':
-                    result = Operation.int_divide(a, b)     
-                
-                elif user_input == 'percent':
-                    result = Operation.percent(a, b)   
-                
-                elif user_input == 'absolute difference':
-                    result = Operation.abs_diff(a, b)       
-                
+                calculation = CalculationFactory.create_calculation(user_input, a, b)
+                result = calculation.compute()
+
                 print(f"Result: {result}")
             
             except OperationError as e:
                 print(f"Error: {e}")
         
         else:   
-            print("Please choose from the list of available commands: add, subtract, multiply, divide, power, root, modulus, integer divide, percent, absolute difference.")
+            print("Please choose from the list of available commands: " \
+            "add, subtract, multiply, divide, power, root, modulus, integer divide, percent, absolute difference.")
 
