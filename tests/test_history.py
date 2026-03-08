893bf0ecbff0699a2e0a213e641310f2cc11b7cf
Matt Howarth <msh32@njit.edu>
2026-03-08 17:24:40 -0400

    Add tests for the empty list, return last calculation, drop last calculation and empty list errors

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,5 +1,7 @@
+import pytest
 from app.calculation import CalculationFactory
 from app.history import History
+from app.exceptions import HistoryError
 
 def test_history_list_is_empty():
     history = History()
@@ -35,3 +37,51 @@ def test_history_clear_list():
     history.clear_history()
 
     assert history.get_history() == []
+
+def test_history_is_empty_true():
+    history = History()
+    assert history.is_empty() is True
+
+def test_history_is_empty_fale():
+    history = History()
+    calculation = CalculationFactory.create_calculation("add", 1, 1)
+
+    history.add_calculation(calculation)
+
+    assert history.is_empty() is False
+
+def test_return_last_calculation():
+    history = History()
+    calculation1 = CalculationFactory.create_calculation("add", 1, 1)
+    calculation2 = CalculationFactory.create_calculation("subtract", 1, 1)
+
+    history.add_calculation(calculation1)
+    history.add_calculation(calculation2)
+
+    assert history.return_last_calculation() == calculation2
+
+def test_return_last_calculation_when_empty_error():
+    history = History()
+
+    with pytest.raises(HistoryError, match="Calculator history is empty"):
+        history.return_last_calculation()
+
+def test_drop_last_calculation():
+    history = History()
+    calculation1 = CalculationFactory.create_calculation("add", 1, 1)
+    calculation2 = CalculationFactory.create_calculation("subtract", 1, 1)
+
+    history.add_calculation(calculation1)
+    history.add_calculation(calculation2)
+
+    dropped = history.drop_last_calculation()
+
+    assert dropped == calculation2
+    assert len(history.get_history()) == 1
+    assert history.return_last_calculation() == calculation1
+
+def test_drop_last_calculation_when_empty_error():
+    history = History()
+
+    with pytest.raises(HistoryError, match="Calculator history is empty"):
+        history.drop_last_calculation()
