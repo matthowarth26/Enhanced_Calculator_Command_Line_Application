4d0cd2df8776817a8fc5e233b4e2af3267820d31
Matt Howarth <msh32@njit.edu>
2026-03-09 01:40:48 -0400

    Update observer logger to use python logging for csv file

--- app/logger.py
+++ app/logger.py
@@ -1,6 +1,7 @@
-import pandas as pd 
-
 from abc import ABC, abstractmethod
+import pandas as pd 
+import logging
+
 from app.calculation import Calculation
 from app.calculator_config import CalculatorConfig
 
@@ -20,8 +21,23 @@ class LoggingObserver(Observer):
     """
     Observer that logs calculation details 
     """
-    def __init__(self) -> None:
+    def __init__(self, log_file: str | None = None) -> None:
+        self.log_file = log_file or CalculatorConfig.get_log_file()
         self.logs: list[str] = []
+
+        self.logger = logging.getLogger(f"calculator_logger_{self.log_file}")
+        self.logger.setLevel(logging.INFO)
+
+        self.logger = logging.getLogger(f"calculator_logger_{self.log_file}")
+        self.logger.setLevel(logging.INFO)
+
+        if not self.logger.handlers:
+            file_handler = logging.FileHandler(self.log_file)
+            formatter = logging.Formatter(
+                "%(asctime)s - %(levelname)s - %(message)s"
+            )
+            file_handler.setFormatter(formatter)
+            self.logger.addHandler(file_handler)
 
     def update(self, calculation: Calculation) -> None:
         """
@@ -29,13 +45,11 @@ class LoggingObserver(Observer):
         """
         log_message = f"Logged calculation: {calculation}"
         self.logs.append(log_message)
+        self.logger.info(log_message)
 
     def get_logs(self) -> list[str]:
-        """"
-        Return all stores log messages
-        """
         return self.logs
-    
+       
 class AutoSaveObserver(Observer):
     """
     Observer that saves history to CSV file
